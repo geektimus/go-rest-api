d2b854f787cc3e8423c893b82de36206fb2e761a
Geektimus <alexander.kno+github@gmail.com>
2017-08-28 17:48:43 -0500

    Clean code, add more handlers

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
@@ -13,10 +12,22 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
+	router.HandleFunc("/games", GameIndex)
+	router.HandleFunc("/games/{gameId}", GameShow)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+func Index(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func GameIndex(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintln(w, "Game Index!")
+}
+
+func GameShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	gameId := vars["gameId"]
+	fmt.Fprintln(w, "Game show:", gameId)
 }
